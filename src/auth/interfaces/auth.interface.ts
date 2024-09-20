d795749a420393fa9104587521a003a5cd224a41
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2024-09-20 15:15:34 +0700

    Features new interface for login and register

---
+++ src/auth/interfaces/auth.interface.ts
@@ -0,0 +1,9 @@
+interface LoginInterfaces {
+    access_token: string; // Define the type of the access_token, for example, a string.
+    refresh_token: string;
+}
+
+interface RegisterInterfaces{
+    created_at: Date
+}
+  
