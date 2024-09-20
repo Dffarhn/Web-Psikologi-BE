c8773ccdd3ce877aa02c6dafacd07041e17df08b
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2024-09-20 07:57:48 +0700

    features auth module for login dan register, and versioning api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { HttpStatus, Injectable, Res } from '@nestjs/common';
+import { RegisterAuthDTO } from './dto/registerAuth.dto';
+import { LoginAuthDTO } from './dto/loginAuth.dto';
+import { ResponseApi } from 'src/common/response/responseApi.format';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  register(registerAuthDTO: RegisterAuthDTO):ResponseApi<String> {
+    return new ResponseApi(HttpStatus.CREATED,"Successfully register user","user register");
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  login(loginAuthDTO:LoginAuthDTO):ResponseApi<String> {
+    return new ResponseApi(HttpStatus.OK,"Successfully login user","user login");
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 }
